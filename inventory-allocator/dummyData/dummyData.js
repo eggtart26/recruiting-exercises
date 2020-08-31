c94f90d7b314e2fb6a81e19bf73e9d4e65fbf355
eggtart26 <justin.guan26@gmail.com>
2020-08-30 22:21:02 -0700

    create dummyData,InventoryAllocator & testing files

---
+++ inventory-allocator/dummyData/dummyData.js
@@ -0,0 +1,9 @@
+module.exports = [
+    { name: 'owd', inventory: { apple: 5 } },
+    { name: 'dm', inventory: { apple: 5 } },
+    { name: 'cho', inventory: { mango: 2 } },
+    { name: 'sfo', inventory: { apple: 5, mango: 2 } },
+    { name: 'bai', inventory: { orange: 1, apple: 5 } },
+    { name: 'yun', inventory: { peach: 2, orange: 4 } },
+  ];
+  
